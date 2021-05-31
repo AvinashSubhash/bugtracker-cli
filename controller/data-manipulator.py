aee8f1c217a5002a4df12093e3183b27cefe6a7d
AvinashSubhash <avinashsubhash19@gmail.com>
2021-05-31 10:51:52 +0530

    added-id-generator-and-tables-created

---
+++ controller/data-manipulator.py
@@ -0,0 +1,42 @@
+
+import time 
+class DataManipulation:
+
+    def __init__(self):
+        self.year = str(time.localtime()[0])
+        self.month = str(time.localtime()[1])
+        self.day = str(time.localtime()[2])
+        self.hour = str(time.localtime()[3])
+        self.minute = str(time.localtime()[4])
+        self.seconds = str(time.localtime()[5])
+        #print(self.seconds)
+
+    def IDGenerattor(self):
+        key =''
+        if int(self.seconds) < 10:
+            key += '0'
+        key += self.seconds
+        if int(self.hour) < 10:
+            key += '0'
+        key += self.hour
+        if int(self.day) < 10:
+            key += '0'
+        key += self.day
+        if int(self.month) < 10:
+            key += '0'
+        key += self.month
+        if int(self.year) < 10:
+            key += '0'
+        key += self.year
+        if int(self.minute) < 10:
+            key += '0'
+        key += self.minute
+
+        return key
+
+sp = DataManipulation()
+print(sp.IDGenerattor())
+
+
+
+
