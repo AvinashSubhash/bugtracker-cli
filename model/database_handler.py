60a904510f90f522e980674ed9e82f0a6637a3b8
AvinashSubhash <avinashsubhash19@gmail.com>
2021-06-11 21:43:39 +0530

    added_commit_command_to_save_entries

--- model/database_handler.py
+++ model/database_handler.py
@@ -18,15 +18,21 @@ class DatabaseHandler:
         return data.fetchall()
 
     def InsertData(self,data):
+        
         print("Database Handler Insert function accessed. .")
         print(data)
         try:
             string1 = str("INSERT INTO BUGTRACKER VALUES("+str(data[5])+", '"+str(data[0])+"', "+str(2)+", '"+str(data[4])+"', "+str(5)+", '"+str(data[1])+"');")
             #print(string1)
             self.db.execute(string1)
+            
         except:
             print("Data could not be added. .")
+        
     def ModifyData(self):
         print("Database Handler Modify function accessed. .")
 
+    def Disconnect(self):
+        self.db.commit()
+        self.db.close()
 
