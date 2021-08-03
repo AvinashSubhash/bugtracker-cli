777f630efda3d564194a8b61a047f2679aaf8b2d
AvinashSubhash <avinashsubhash19@gmail.com>
2021-08-03 11:42:10 +0530

    var_Error_removed

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo ""
 echo "Adding Shortcut to bashrc . ."
 echo ""
 curr_path=$PWD;
-install_command="alias bugdtracker=\"cd $curr_path ; python3 main.py ; cd ~ \" "
+install_command="alias bugtracker=\"cd $curr_path ; python3 main.py ; cd ~ \" "
 #echo $install_command
 echo $install_command >> ~/.bashrc
 source ~/.bashrc
