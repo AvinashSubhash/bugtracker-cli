7026ddbcb621e2165fb28e79a172c4a27c9dcb21
AvinashSubhash <avinashsubhash19@gmail.com>
2023-01-02 23:35:40 +0530

    removed-bug-of-path-not-found

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 clear
 echo -e "Welcome to Bugtracker v1.0!\n"
 
-
-if [ ! -d "/home/$USER/.bugtracker-files" ]
+if [ -d "/home/$USER/.bugtracker-files" ]
 then
-    mkdir /home/$USER/.bugtracker-files
-    cp -R $PWD /home/$USER/.bugtracker-files/
-    export PATH=$PATH:/home/$USER/.bugtracker-files/bugtracker-cli/
-    echo "PATH=\$PATH:/home/$USER/.bugtracker-files/bugtracker-cli/" >> /home/$USER/.bashrc
-    #echo "Welcome to Bugtracker v1.0!"
-    #echo ""
-    #echo "Installation Successfull ! !"
-    #echo ""
-    exit
+    yes | rm -R /home/$USER/.bugtracker-files
 fi
+mkdir /home/$USER/.bugtracker-files
+cp -R $PWD /home/$USER/.bugtracker-files/
+export PATH=$PATH:/home/$USER/.bugtracker-files/bugtracker-cli/
+echo "PATH=\$PATH:/home/$USER/.bugtracker-files/bugtracker-cli/" >> /home/$USER/.bashrc
+#echo "Welcome to Bugtracker v1.0!"
+#echo ""
+#echo "Installation Successfull ! !"
+#echo ""
+exit
